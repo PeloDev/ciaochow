d0d5a5596e9e51acbc534fcb932666f2cc75526a
Boipelo Zuma <pelothedeveloper@gmail.com>
2024-02-29 12:32:38 +0200

    add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center bg-white">
+        <p>Page Not Found</p>
+      </main>
+    );
+  }
+  
