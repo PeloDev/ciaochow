9116f86c05d1f557daca65cd6e73158c19ec0598
Boipelo Zuma <pelothedeveloper@gmail.com>
2024-03-02 13:01:56 +0200

    add SVG export barrel in to simplify imports

---
+++ src/app/assets/vectors/index.tsx
@@ -0,0 +1,13 @@
+import womanWithGroceries from "./woman-groceries.svg";
+import manWithGroceries from "./man-groceries.svg";
+import chevronLeft from "./chevron-left.svg";
+import passwordHidden from "./password-hidden.svg";
+import passwordVisible from "./password-visible.svg";
+
+export {
+  womanWithGroceries,
+  manWithGroceries,
+  chevronLeft,
+  passwordHidden,
+  passwordVisible,
+};
