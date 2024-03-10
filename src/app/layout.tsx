6384db5c8aba475e0b875354e8c6e50becdc06c5
Boipelo Zuma <pelothedeveloper@gmail.com>
2024-03-10 12:01:52 +0200

    setup toast messages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
           className={`${inter.className} mx-auto bg-ccGray flex justify-center`}
         >
           <main className="flex min-h-screen flex-col items-center max-w-[500px] w-full justify-center bg-ccGreen text-white">
+            <Toaster position="top-center" />
             {children}
           </main>
         </body>
