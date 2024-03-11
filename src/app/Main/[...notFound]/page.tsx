dbf2e735d2dbf59c47c5f5f48520e3384ba6d72f
Boipelo Zuma <pelothedeveloper@gmail.com>
2024-03-11 05:13:44 +0200

    fix build issues with Main

--- src/app/Main/[...notFound]/page.tsx
+++ src/app/Main/[...notFound]/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 import { usePathname } from "next/navigation";
-import Main from "../page";
+import MainComponent from "../MainComponent";
 
 export default function NotFoundCatchAllPage() {
   const pathname = usePathname();
 
   if (pathname === "/main/description") {
-    return <Main defaultTab="description" />;
+    return <MainComponent defaultTab="description" />;
   } else if (pathname === "/main/nutritionalFacts") {
-    return <Main defaultTab="nutritionalFacts" />;
+    return <MainComponent defaultTab="nutritionalFacts" />;
   }
   return <p>Page Not Found</p>;
 }
